050e2a41115003a77addd88ab9c4a976b2df5f1d
atul0636@gmail.com <RGAak@0636>
2023-11-26 03:51:06 +0530

    Base created

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+// Function to change background color
+function changeBackgroundColor() {
+    const aboutSection = document.getElementById('about');
+    if (aboutSection) {
+        aboutSection.style.backgroundColor = '#1e90ff'; // Change to neon royal blue
+    }
+}
